3d8912d086c4f0b7d98f64d680c4558491eb39da
Christian Johansen <christian@cjohansen.no>
2019-01-19 00:05:09 +0100

    Dice cards

--- devcards/yahtzee/cards/dice_cards.cljs
+++ devcards/yahtzee/cards/dice_cards.cljs
@@ -2,5 +2,22 @@
   (:require [dumdom.devcards :refer-macros [defcard]]
             [yahtzee.components.die :as die]))
 
-(defcard die-one
-  (die/die 1))
+(defcard dice
+  [:div {:style {:display "grid"
+                 :grid-template-columns "1fr 1fr 1fr"}}
+   [:div {:className "mod"} (die/die {:value 1})]
+   [:div {:className "mod"} (die/die {:value 2})]
+   [:div {:className "mod"} (die/die {:value 3})]
+   [:div {:className "mod"} (die/die {:value 4})]
+   [:div {:className "mod"} (die/die {:value 5})]
+   [:div {:className "mod"} (die/die {:value 6})]])
+
+(defcard colored-dice
+  [:div {:style {:display "grid"
+                 :grid-template-columns "1fr 1fr 1fr"}}
+   [:div {:className "mod"} (die/die {:color "red" :value 1})]
+   [:div {:className "mod"} (die/die {:color "red" :value 2})]
+   [:div {:className "mod"} (die/die {:color "red" :value 3})]
+   [:div {:className "mod"} (die/die {:color "red" :value 4})]
+   [:div {:className "mod"} (die/die {:color "red" :value 5})]
+   [:div {:className "mod"} (die/die {:color "red" :value 6})]])
--- src/yahtzee/components/die.cljs
+++ src/yahtzee/components/die.cljs
@@ -1,5 +1,36 @@
 (ns yahtzee.components.die
   (:require [dumdom.core :refer [defcomponent]]))
 
-(defcomponent die [value]
-  [:div {} "Value: " value])
+(defn die-styles [color x y]
+  {:background color
+   :border-radius "50%"
+   :width "22%"
+   :padding-bottom "22%"
+   :position "absolute"
+   :left (str x "%")
+   :top (str y "%")
+   :transform "translate(-50%, -50%)"})
+
+(def x 22)
+(def y 22)
+
+(defcomponent die [{:keys [color value]}]
+  (let [color (or color "#000")]
+    [:div {:style {:border (str "1vw solid " color)
+                   :border-radius "10%"}}
+     [:div {:style {:position "relative"
+                    :padding-bottom "100%"}}
+      (when (#{1 3 5} value)
+        [:div {:style (die-styles color 50 50)}])
+      (when (#{2 3 4 5 6} value)
+        [:div {:style (die-styles color x y)}])
+      (when (#{2 3 4 5 6} value)
+        [:div {:style (die-styles color (- 100 x) (- 100 y))}])
+      (when (#{4 5 6} value)
+        [:div {:style (die-styles color (- 100 x) y)}])
+      (when (#{4 5 6} value)
+        [:div {:style (die-styles color x (- 100 y))}])
+      (when (= 6 value)
+        [:div {:style (die-styles color x 50)}])
+      (when (= 6 value)
+        [:div {:style (die-styles color (- 100 x) 50)}])]]))
