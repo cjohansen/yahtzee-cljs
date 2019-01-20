c223074af6ec442c8d8c3307363f40847849fa3d
Christian Johansen <christian@cjohansen.no>
2019-01-20 21:56:29 +0100

    Remove unused file

--- src/yahtzee/actions.cljs
+++
@@ -1,9 +0,0 @@
-(ns yahtzee.actions
-  (:require [yahtzee.game :as yahtzee]))
-
-(defn trigger-action [game action & args]
-  (case action
-    :roll (apply swap! game yahtzee/roll args)
-    :hold (apply swap! game yahtzee/hold args)
-    :release (apply swap! game yahtzee/release args)
-    :score (apply swap! game yahtzee/score args)))
